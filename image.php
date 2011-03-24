3e4abb1af14382129ee4df028171c6110fa1f26a
thingsinjars <simon@angryrobotzombie.com>
2011-03-24 19:00:54 +0000

    Initial version

---
+++ image.php
@@ -0,0 +1,31 @@
+<?php
+$filename = $_GET['image'];
+
+list($width, $height) = getimagesize($filename);
+
+$src_image = imagecreatefrompng($filename);
+
+// Create a new true color image
+$dst_image = imagecreatetruecolor($width, $height);
+
+//Save all alpha info when writing out
+
+$hot_pink = imagecolorallocate($dst_image, 255, 105, 180);
+imagecolortransparent($dst_image, $hot_pink);
+imagefill($dst_image, 0, 0, $hot_pink);
+
+
+//imagealphablending($dst_image, false);
+//imagesavealpha($dst_image, true);
+imagecopy($dst_image, $src_image, 0, 0, 0, 0, $width, $height);
+
+// Convert to palette-based with no dithering and 255 colors with alpha
+imagetruecolortopalette($dst_image, false, 255);
+
+imagecolormatch($src_image, $dst_image);
+
+
+// Output the image
+header('Content-type: image/png');
+imagepng($dst_image);
+imagedestroy($dst_image);
---
+++ upload.php
@@ -0,0 +1,45 @@
+<?php
+
+class Uploader {
+	private $fileName;
+	private $contentLength;
+	private $path;
+	private $log;
+	
+	public function __construct($uploads) {
+		$this->path=$uploads;
+		$log=fopen($this->path . 'log.txt','w');
+		if (array_key_exists('HTTP_X_FILE_NAME', $_SERVER) && array_key_exists('CONTENT_LENGTH', $_SERVER)) {
+			$this->fileName = $_SERVER['HTTP_X_FILE_NAME'];
+			fwrite($log,"Receiving '" .  $this->fileName . "'\n");
+			$this->contentLength = $_SERVER['CONTENT_LENGTH'];
+		} else {
+			throw new Exception("Error retrieving headers");
+		}
+		fclose($log);
+	}
+    
+    public function receive() {
+        if (!$this->contentLength > 0) {
+            throw new Exception('No file uploaded!');
+        }
+		
+        file_put_contents(
+            $this->path . $this->fileName,
+            file_get_contents("php://input")
+        );
+        
+        return true;
+    }
+}
+
+$uploads="tmp/";
+try {
+	$ft = new Uploader($uploads);
+	$ft->receive();
+} catch (Exception $e) {
+	$fd=fopen($uploads . 'err.txt','w');
+    fwrite($fd,'Caught exception: ' .  $e->getMessage() . "\n");
+	fclose($fd);
+}
+?>
