88796469f94aed6d57543cd86b87c5696d96861c
thingsinjars <simon@angryrobotzombie.com>
2011-03-28 19:48:42 +0100

    Added a better 404 check to avoid hanging the browser

--- js/8bit.js
+++ js/8bit.js
@@ -1,29 +1,36 @@
 $(function() {
-	$("#dropzone").dropzone({
-		url : "upload.php",
-		printLogs : true,
-		uploadRateRefreshTime : 500,
-		numConcurrentUploads : 1
-	});
+  $("#dropzone").dropzone({
+    url : "upload.php",
+    printLogs : true,
+    uploadRateRefreshTime : 500,
+    numConcurrentUploads : 1
+  });
   $('#questions, #about_close').click(function() {$('#about').toggle();})
 });
 
 $.fn.dropzone.newFilesDropped = function() {
-  $('#dropzone').empty().append($('<img src="images/spinner.gif" width="16" height="16" />').css({margin:'50px'}));
+  $('#dropzone').empty().append($('<img src="images/spinner.gif" />').css({margin:'50px'}));
   $('#downloadables').empty();
 };
 $.fn.dropzone.uploadFinished = function(fileIndex, file, time) {
-	// This is one slightly nasty hack stuck in to get round 
-	// the fact that the upload event fires after the file 
-	// has been moved to the server but might fire before the 
-	// PHP internals have moved the file from the temporary 
-	// upload folder to the intended destination.
-	while(!UrlExists('tmp/' + file.fileName)) {} 
-  $('#dropzone').empty().append('<img src="tmp/' + file.fileName + '">');
-  $('#downloadables').append('<img src="image.php?image=tmp/' + file.fileName + '">');
+  // This is one slightly nasty hack stuck in to get round
+  // the fact that the upload event fires after the file
+  // has been moved to the server but might fire before the
+  // PHP internals have moved the file from the temporary
+  // upload folder to the intended destination.
+  delayThenShowImages(file.fileName, 10);
 };
+
+function delayThenShowImages(fileName, i) {
+  if( !UrlExists('tmp/' + fileName) && i-->0 ) {
+    setTimeout(function() {delayThenShowImages(fileName, i);}, 20);
+  } else {
+    $('#dropzone').empty().append('<img src="tmp/' + fileName + '">');
+    $('#downloadables').append('<img src="image.php?image=tmp/' + fileName + '">');
+  }
+}
 function UrlExists(url) {
- 	var http = new XMLHttpRequest();
+  var http = new XMLHttpRequest();
   http.open('HEAD', url, false);
   http.send();
   return http.status!=404;
